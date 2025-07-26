3c2d40c9f8309affb291d9481e0c186ec46bf92e
DevoScientist <anubhab13rta@gmail.com>
2025-07-27 03:52:11 +0530

    Passing data using ngOnInit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,19 @@
+import { ɵnormalizeQueryParams } from '@angular/common';
 import { Component } from '@angular/core';
+import { Route, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  constructor(private router:Router){}
+
+  // goToProfile(){
+  //   this.router.navigate(['profile'],  {ɵnormalizeQueryParams{name : 'Milk-way'}})
+  // }
 
 }
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,22 @@
+import { compileNgModule } from '@angular/compiler';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+
+  userName?:string | null='';
+
+  constructor(private route:ActivatedRoute){ }
+
+  ngOnInit():void{  // Automaticallu=y caled when the website is invoked
+    // this.userName=this.route.snapshot.paramMap.get('name');
+    // console.log(this.userName);
+    
+  }
+}
