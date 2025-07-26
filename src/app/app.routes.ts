0ebf69fb18b76560d60f5fc049bc42a214997588
DevoScientist <anubhab13rta@gmail.com>
2025-07-27 02:30:18 +0530

    added home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path:'about', component:AboutComponent},
     {path:'login', component:LoginComponent},
-    {path:'contact', component:ContactComponent}
+    {path:'contact', component:ContactComponent},
+    {path:'',component:HomeComponent}
 ];
