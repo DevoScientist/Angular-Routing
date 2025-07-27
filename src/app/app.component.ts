88310ed3d2aaecc322946599c01ff3370bc24034
DevoScientist <anubhab13rta@gmail.com>
2025-07-27 21:38:22 +0530

    completed tutorial: now hop on to project...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { CurrencyConverterPipe } from './pipe/currency-converter.pipe';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink],
+  imports: [CommonModule, CurrencyConverterPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular-Routing';
+  // title = 'hello milky way!!'
+  // name = "DEVOSCIENTIST"
+
+  // date = new Date()
+
+  // amount = 10
+
+  prodctList:any
+  constructor(private prodService : ProductService){
+
+  }
+
+  ngOnInit(){
+    this.prodService.getProductList().subscribe((data:any)=>{
+      console.log(data);
+      
+      this.prodctList=data.products
+    })
+  }
+
+
 }
